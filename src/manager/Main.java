d0fe5d273cd365d2d57b606599e5e64b7f73a342
Oprea-Groza Gabriel <56924647+GabrielOprea@users.noreply.github.com>
2021-08-08 01:46:30 +0300

    Update Main.java

--- src/manager/Main.java
+++ src/manager/Main.java
@@ -17,7 +17,7 @@ public class Main {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/studentmanaging", "root",
-                    "aDesea132_");
+                    "pass");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select * from students");
             while (rs.next())
