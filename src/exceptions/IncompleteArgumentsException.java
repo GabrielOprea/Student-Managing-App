138a60fa86b0c8a9d5d16ef626a1861ecbf937da
Oprea-Groza Gabriel <56924647+GabrielOprea@users.noreply.github.com>
2021-08-19 15:54:39 +0300

    Delete src directory

--- src/exceptions/IncompleteArgumentsException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class IncompleteArgumentsException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Incomplete arguments!";
-    }
-}
--- src/exceptions/NotEnoughArgumentsException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class NotEnoughArgumentsException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Not enough arguments!";
-    }
-}
